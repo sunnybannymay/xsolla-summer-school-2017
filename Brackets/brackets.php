c2cf2292b99a84e9a35050adb69dc76ef8329f4c
sunnybannymay <blockchain1234@mail.ru>
2017-07-15 21:42:49 +0500

    Update brackets.php

--- Brackets/brackets.php
+++ Brackets/brackets.php
@@ -3,14 +3,18 @@
 class Brackets
 {
   public function isBracketSequenceCorrect($str) {
+	  //создаем стек, куда будем записывать строки
 	  $array=array();
-	  
+	  //посимвольно считываем введенную строку
 	for($i=0;$i<strlen($str);$i++)
-	{		
+	{	
+		//если стек пуст, записываем символ в стек
 		if(count($array)==0){
 			array_push($array,$str[$i]);
 		}		
 		else{
+		//если последний символ в стеке, открывающая скобка, а текущий символ закрывающая скобка того же формата,
+		//то извлекаем последний символ из стека	
 			if($array[count($array)-1]=="[")		
 				if($str[$i]=="]")
 					array_pop($array);
@@ -30,6 +34,7 @@ class Brackets
 						array_push($array,$str[$i]);		
 		}
 	}
+	  //если по окончанию прохода стек оказался пуст, значит скобочная последовательность была правильная 
 	if(count($array)==0){
 		print_r(true);
 		return true;
